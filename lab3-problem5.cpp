d7bb7c6165b4759f0e06bef0ad8c600d93dcc69b
plseetiong <zapanta.pierre@outlook.com>
2016-09-26 17:24:38 +0800

    Add files via upload

--- lab3-problem5.cpp
+++ lab3-problem5.cpp
@@ -1,15 +1,26 @@
 #include<iostream>
 #include<cstring>
-#include<ctype.h>
+#include<cctype>
+#include<cmath>
 using namespace std;
 
-main(){
+int main(){
 	char str[50];
-	char toUpper;
-	 cout<<"Enter some string: ";
-	 cin.getline(str, 50);
-	 for(int i=0; i<strlen(str);i++){ //to upper function
-	 	toUpper = toupper(str[i]);
-	 	cout<<toUpper;
-	 }
+	cout<<"Enter some string: ";
+	cin.getline(str, 50);
+	
+	str[0]=toupper(str[0]);
+	
+	for(int i=1; i<strlen(str);i++){
+		if(isspace(str[i])){
+			i++;
+			str[i]=toupper(str[i]);
+		}
+		else{
+			str[i]=tolower(str[i]);
+		}
+	}
+	cout<<str;
+	
+	return 0;
 }
